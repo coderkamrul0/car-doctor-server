64833574cbd3dfd8d8e3b56dc0c7be6c77f6b7f8
Kamrul Hasan <kamrul444216@gmail.com>
2023-05-11 04:02:43 +0600

    service checkout data find

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -27,6 +27,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const serviceCollection = client.db('carDoctor').collection('services');
+
+    app.get('/services', async(req,res)=>{
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+    app.get('/services/:id', async(req, res)=>{
+       const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+
+      const options = {
+        projection: { title: 1, price: 1, service_id: 1}
+      }
+      const result = await serviceCollection.findOne(query,options);
+      res.send(result);
+    })
     
 
 
