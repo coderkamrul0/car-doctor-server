f95121ad77ba6326bab365f31d9b464497d6ba32
Kamrul Hasan <kamrul444216@gmail.com>
2023-05-11 01:09:08 +0600

    Mongodb connect

---
+++ index.js
@@ -0,0 +1,86 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+// 7ppLNacCSl6I7sJF
+// car-doctor
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.mjrrjle.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    
+
+
+
+
+
+
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("Car doctor server is running");
+})
+
+app.listen(port, ()=>{
+    console.log(`Car doctor server is running: ${port}`);
+})
